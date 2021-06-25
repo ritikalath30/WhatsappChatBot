6a5808b563587bdbe77fb34657d1dee9e16cd738
Ritika Lath <lathritika2@gmail.com>
2021-06-25 14:51:40 +0530

    changes in app.py

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ from flask import Flask,request
 from twilio.twiml.messaging_response import MessagingResponse
 
 from utils import fetch_reply
+
 app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
 
-
 @app.route("/sms", methods=['POST'])
 def sms_reply():
     msg = request.form.get("Body")
-    phone_no = request.form.get('From')
+    phone_no = request.form.get("From")
     reply = fetch_reply(msg,phone_no)
     resp = MessagingResponse()
     resp.message(reply)
